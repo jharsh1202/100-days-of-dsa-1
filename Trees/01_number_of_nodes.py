f0794e5980c60ee8b413d53ed3e666ed7f4b49e1
Harshit Jain <harshitjain@Harshits-MacBook-Pro.local>
2023-10-27 03:36:32 +0530

    Automated commit and push

---
+++ Trees/01_number_of_nodes.py
@@ -0,0 +1,4 @@
+# https://www.codingninjas.com/studio/problems/number-of-nodes_8162204?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf 
+
+def numberOfNodes(N : int) -> int:
+    return (2**(N-1))
---
+++ Trees/02_create_binary_tree.py
@@ -0,0 +1,22 @@
+# REDO
+# Create Binary Tree https://www.codingninjas.com/studio/problems/create-binary-tree_8360671?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+from typing import List
+class Node:
+    def __init__(self, data=0, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def insertNode(arr, i, n):
+    root=None
+    if i < n:
+        root = Node(arr[i]) 
+        root.left = insertNode(arr, 2 * i + 1, n)
+        root.right = insertNode(arr, 2 * i + 2, n)
+    return root
+    
+
+def createTree(arr: List[int]) -> Node:
+    if not arr: return None 
+    return insertNode(arr, 0, len(arr))
+    
