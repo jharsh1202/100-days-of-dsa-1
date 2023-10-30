7cfb2ce2d3ebbf97ab083a877a95fd06bf89b5f8
Harshit Jain <harshitjain@Harshits-MacBook-Pro.local>
2023-10-30 12:00:00 +0530

    Automated commit and push

--- Trees/traversal/05_preorder_iterative.cpp
+++ Trees/traversal/05_preorder_iterative.cpp
@@ -1,24 +1,21 @@
-/************************************************************
-
-    Following is the TreeNode class structure
-
-    template <typename T>
-    class TreeNode {
-       public:
-        T data;
-        TreeNode<T> *left;
-        TreeNode<T> *right;
-
-        TreeNode(T data) {
-            this->data = data;
-            left = NULL;
-            right = NULL;
-        }
-    };
-
-************************************************************/
 #include <stack>
 #include <vector>
+
+
+template <typename T>
+class TreeNode {
+    public:
+    T data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
 
 vector<int> preOrder(TreeNode<int>* root){
     stack<TreeNode<int>*> stack;
